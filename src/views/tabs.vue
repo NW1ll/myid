95871b8c15c8e46f30774e6b0b708ddfacf16722
NW1ll <906950106@qq.com>
2023-02-15 21:51:58 +0800

    2.3

--- src/views/tabs.vue
+++ src/views/tabs.vue
@@ -2,38 +2,36 @@
 	<div class="container">
 		<el-tabs v-model="message">
 			<el-tab-pane :label="`已验证属性的消息`" name="first">
-				<el-table :data="state.unread" :show-header="false" style="width: 100%">
-					<el-table-column>
-						<template #default="scope">
-							<span class="message-title">{{ scope.row.title }}</span>
+				<vxe-table ref="xTable" :data="state1.unread" :show-header="false" >
+					<vxe-column field="name">
+						<template #default="{row}">
+							<span class="message-title">{{ row.title }}</span>
 						</template>
-					</el-table-column>
-					<el-table-column prop="date" width="180"></el-table-column>
-					<el-table-column width="120">
-						<template #default="scope">
+					</vxe-column>
+					<vxe-column width="120">
+						<template #default="{row}">
 							<el-button  @click="forward">同意，前往</el-button>
-              <el-button type="danger" style="margin-top: 10px" @click="clearData1">拒绝</el-button>
+              <el-button type="danger" style="margin-top: 10px" @click="deleteRowEvent(row)">拒绝</el-button>
 						</template>
-					</el-table-column>
-				</el-table>
+					</vxe-column>
+				</vxe-table>
 
 			</el-tab-pane>
 			<el-tab-pane :label="`未验证属性的消息`" name="second">
 				<template v-if="message === 'second'">
-					<el-table :data="state.read" :show-header="false" style="width: 100%">
-						<el-table-column>
-							<template #default="scope">
-								<span class="message-title">{{ scope.row.title }}</span>
+					<vxe-table ref="xTable2" :data="state2.read" :show-header="false" style="width: 100%">
+						<vxe-column>
+							<template #default="{row}">
+								<span class="message-title">{{ row.title }}</span>
 							</template>
-						</el-table-column>
-						<el-table-column prop="date" width="150"></el-table-column>
-						<el-table-column width="120">
-							<template #default="scope">
+						</vxe-column>
+						<vxe-column width="120">
+							<template #default="{row}">
 								<el-button  @click="forward">前往添加</el-button>
-                <el-button type="danger" style="margin-top: 10px" @click="clearData2()" >拒绝</el-button>
+                <el-button type="danger" style="margin-top: 10px" @click="deleteRowEvent2(row)">拒绝</el-button>
 							</template>
-						</el-table-column>
-					</el-table>
+						</vxe-column>
+					</vxe-table>
 
 				</template>
 			</el-tab-pane>
@@ -41,47 +39,66 @@
 	</div>
 </template>
 
-<script setup lang="ts" name="tabs">
-import { ref, reactive } from 'vue';
-import { useRouter } from 'vue-router';
-import {VXETable} from "vxe-table";
-const router = useRouter();
-const message = ref('first');
-const state = reactive({
-	unread: [
-		{
-			date: '2018-04-19 20:00:00',
-			title: 'www.baidu.com请求您的属性'
-		},
-	],
-	read: [
-		{
-			title: 'www.google.com请求访问您的属性'
-		}
-	],
-	recycle: [
-		{
-			date: '2018-04-19 20:00:00',
-			title: '【系统通知】该系统将于今晚凌晨2点到5点进行升级维护'
-		}
-	]
-});
-const forward = () => {
-  router.push('/editinfo')
-}
-const clearData1 = () =>{
-  state.unread[0] = {
-    date: '2018-04-19 20:00:00',
-    title: ''
+<script  lang="ts" name="savedata">
+import { ref, reactive,defineComponent } from 'vue';
+import {VxeGridInstance,  VxeGridProps, VXETable } from 'vxe-table'
+import {useRouter} from "vue-router";
+export default defineComponent({
+  setup () {
+    const xTable =ref<VxeGridInstance>()
+    const xTable2 =ref<VxeGridInstance>()
+
+    const deleteRowEvent = async (row: any) => {
+      const $table = xTable.value
+      const type = await VXETable.modal.confirm('您确定要删除该数据?')
+      if (type === 'confirm') {
+        await $table!.remove(row)
+      }
+    }
+
+    const deleteRowEvent2 = async (row: any) => {
+      const $table1 = xTable2.value
+      const type = await VXETable.modal.confirm('您确定要删除该数据?')
+      if (type === 'confirm') {
+        await $table1!.remove(row)
+      }
+    }
+
+    const router = useRouter();
+    const state1 = reactive({
+      unread: [
+        {
+          title: 'www.baidu.com请求您的属性'
+        }
+      ]
+    });
+
+    const state2 = reactive({
+      read: [
+        {
+          title: 'www.baidu.com请求您的属性'
+        }
+      ]
+    });
+
+    const forward = () => {
+      router.push('/editinfo')
+    }
+
+    return {
+      deleteRowEvent,
+      deleteRowEvent2,
+      state1,
+      state2,
+      forward,
+      xTable,
+      xTable2
+    }
   }
-}
+})
 
 
-const clearData2 = () =>{
-  state.read[0] = {
-    title: ''
-  }
-}
+
 </script>
 
 <style>
